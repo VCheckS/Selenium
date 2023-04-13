7393124708323914917680bea50c4c6080487c99
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-12 23:03:00 -0700

    Positive test seted

--- src/test/java/TestWebDriver.java
+++ src/test/java/TestWebDriver.java
@@ -43,14 +43,14 @@ public class TestWebDriver {
     public void shouldTestV1() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+9222650601");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79222650601");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button__text")).click();
-        driver.findElement(By.cssSelector("button.button")).click();
+        driver.findElement(By.className("button__text")).click();
+//        driver.findElement(By.cssSelector("button.button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+//        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
-
 
     }
 }
